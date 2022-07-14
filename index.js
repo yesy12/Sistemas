e1835dcf834cb59fe2fce1c1a68512f9fb0882f4
Alison <alisonvieira.av29@gmail.com>
2022-07-14 01:44:08 -0300

    Partials Footer.ejs and Header.ejs, create page UserIndex UserLogin UserRegistration

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 9000;
+ 
+app.set("view engine","ejs")
 
+//Routes
 app.get("/user",(req,res)=>{
-    res.send("Hello World");
+    res.render("user/",{
+        "title" : "Usuario"
+    });
+})
+
+app.get("/user/login",(req,res)=>{
+    res.render("user/login",{
+        "title" : "Login"
+    })
+})
+
+app.get("/user/registration",(req,res)=>{
+    res.render("user/registration",{
+        "title" : "Cadastro"
+    })
 })
 
 app.listen(port,()=>{
