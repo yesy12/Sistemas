8133b9740fdf6d8fa34c3dec673f72e40eb6ce60
Alison <alisonvieira.av29@gmail.com>
2022-07-14 02:44:00 -0300

    global, error and right on connector on database

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 require("dotenv").config()
 const {MongoClient} = require("mongodb");
+
 const username = process.env.username_mongoDb;
 const password = process.env.password_mongoDb;
+const dbName = process.env.dbName_mongoDb;
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.nth3w.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
@@ -9,6 +11,12 @@ const client = new MongoClient(uri, {
     useUnifiedTopology:true,
 })
 
-client.connect(()=>{
-    console.log("Connected on Database")
+client.connect((error,connection)=>{
+    if(error){
+        return console.log(error)
+    }
+    else if(connection){
+        console.log("Connected on Database")
+        global.connection = connection.db(dbName)
+    }
 })
