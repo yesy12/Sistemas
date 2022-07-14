fa55ee48a549e46249e4ac5892e293ad630d7ce0
Alison <alisonvieira.av29@gmail.com>
2022-07-14 03:00:20 -0300

    remove mongodb

--- db.js
+++ db.js
@@ -1,22 +1,12 @@
 require("dotenv").config()
-const {MongoClient} = require("mongodb");
+const mongoose = require("mongoose");
 
 const username = process.env.username_mongoDb;
 const password = process.env.password_mongoDb;
 const dbName = process.env.dbName_mongoDb;
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.nth3w.mongodb.net/?retryWrites=true&w=majority`
-const client = new MongoClient(uri, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
 
-client.connect((error,connection)=>{
-    if(error){
-        return console.log(error)
-    }
-    else if(connection){
-        console.log("Connected on Database")
-        global.connection = connection.db(dbName)
-    }
+const mongoonse = mongoose.connect(`${uri}/${dbName}`,()=>{
+    console.log("Connection on Database")
 })
