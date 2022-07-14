eec670e48c8ad5969325d268c6ad27b72b248e57
Alison <alisonvieira.av29@gmail.com>
2022-07-14 01:10:04 -0300

    ejs, bodyparser, /user

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
+app.get("/user",(req,res)=>{
+    res.send("Hello World");
 })
 
 app.listen(port,()=>{
