6c550a4c460f99e7776631c4ef722d6fce3a801d
Alison <alisonvieira.av29@gmail.com>
2022-07-14 02:04:57 -0300

    Register

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 9000;
  
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.set("view engine","ejs")
+
 
 //Routes
 app.get("/user",(req,res)=>{
@@ -21,6 +26,10 @@ app.get("/user/registration",(req,res)=>{
     res.render("user/registration",{
         "title" : "Cadastro"
     })
+})
+
+app.post("/user/registration",(req,res)=>{
+    res.send(req.body)
 })
 
 app.listen(port,()=>{
