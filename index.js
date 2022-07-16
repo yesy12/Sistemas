a409617a9d31cf0a07efdb05cbc5944300b1285d
Alison <alisonvieira.av29@gmail.com>
2022-07-16 20:06:51 -0300

    verification of inputs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const User = require("./models/User");
 const app = express();
 const port = 9000;
  
@@ -9,6 +10,8 @@ app.set("view engine","ejs")
 
 require("./db");
 
+
+const UserSchema = require("./models/User")
 
 //Routes
 app.get("/user",(req,res)=>{
@@ -29,8 +32,72 @@ app.get("/user/registration",(req,res)=>{
     })
 })
 
-app.post("/user/registration",(req,res)=>{
-    res.send(req.body)
+app.post("/user/registration",async(req,res)=>{
+    const {lName,lLastName, lCpf, lDateBirthday, lTelephone} = req.body;
+
+    errors = [];
+    const regex_number = new RegExp("[0-9]")
+    const regex_text = new RegExp("[A-z]")
+
+    //Name
+    if(!lName || typeof lName == undefined || lName == null || regex_number.test(lName)){
+        errors.push({
+            text : "Name Invalid"
+        });
+    }
+
+    //LastName
+    if(!lLastName || typeof lLastName == undefined || lLastName == null || regex_number.test(lLastName)){
+        errors.push({
+            text : "Last Name Invalid"
+        });
+    }
+
+    //Cpf
+    if(!lCpf || typeof lCpf == undefined || lCpf == null || regex_text.test(lCpf)){
+        errors.push({
+            text : "CPF Invalid"
+        });
+    }
+    if(lCpf.length > 11 || lCpf.length < 11){
+        errors.push({
+            text : "CPF is wrong in the length"
+        })
+    }
+
+
+    //lDateBirthday
+    const date = lDateBirthday.split("-")
+    console.log(date)
+    if(!lDateBirthday || typeof lDateBirthday == undefined || lDateBirthday == null || regex_text.test(lDateBirthday)){
+        errors.push({
+            text : "Date Birthday Invalid"
+        });
+    }
+
+    if(lDateBirthday.length > 10 || lDateBirthday.length < 10 || date[0] > new Date().getFullYear() || date[1] > (new Date().getMonth()+1) ||date[2] > (new Date().getDate()+1)){
+        errors.push({
+            text : "Date Birthday wrong"
+        })
+    }
+
+    //lTelephone
+    if(!lTelephone || typeof lTelephone == undefined || lTelephone == null || regex_text.test(lTelephone)){
+        errors.push({
+            text : "Telephone Invalid"
+        });
+    }
+
+
+    if(errors.length > 0){
+        res.send({errors})
+    }
+    else{
+        res.send(req.body)
+    }
+
+    
+    
 })
 
 app.listen(port,()=>{
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose= require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    "nameComplete": String,
+    "lastName": String,
+    "cpf": Number,
+    "age":Number,
+    "dateBirthday": Date,
+    "status": {
+        type: Boolean,
+        default: true,
+    },
+    "tierHierarchy": {
+        type :  Number,
+        default: 0,
+    },
+    "dateCreation":{
+        type: Date,
+        default: Date.now(),
+    },
+    "dateUpdate":{
+        type: Date,
+        default: Date.now(),
+    }
+})
+
+module.exports = mongoose.model("User",UserSchema)
