6028be3c013b41efd1035e55c12953755b269f28
Alison <alisonvieira.av29@gmail.com>
2022-07-16 18:03:52 -0300

    db

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 require("dotenv").config()
 const mongoose = require("mongoose");
 
-const username = process.env.username_mongoDb;
-const password = process.env.password_mongoDb;
-const dbName = process.env.dbName_mongoDb;
+const uri = process.env.uriMongoDb
 
-const uri = `mongodb+srv://${username}:${password}@cluster0.nth3w.mongodb.net/?retryWrites=true&w=majority`
-
-const mongoonse = mongoose.connect(`${uri}/${dbName}`,()=>{
-    console.log("Connection on Database")
+const db = mongoose.connect(uri,
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+    },
+    (error, connection)=>{
+        if(error){
+            console.log(error)
+            return 
+        }
+        else if(connection){
+            console.log("Connection on Database")
+        }
 })
